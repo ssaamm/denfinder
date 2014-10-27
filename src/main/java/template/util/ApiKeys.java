015f25083cf035f2d63766a64ebaf858442ea8bf
ssaamm <github@samueltaylor.org>
2014-10-27 11:38:25 -0500

    Use environment variables for API keys

---
+++ src/main/java/template/util/ApiKeys.java
@@ -0,0 +1,12 @@
+package template.util;
+
+public class ApiKeys {
+	public static final String cEducationKey = System.getenv("DF_EDUCATION_KEY") == null ? ""
+			: System.getenv("DF_EDUCATION_KEY"),
+			cCensusKey = System.getenv("DF_CENSUS_KEY") == null ? "" : System
+					.getenv("DF_CENSUS_KEY"),
+			cGovCensusKey = System.getenv("DF_GOV_CENSUS_KEY") == null ? "" : System
+					.getenv("DF_GOV_CENSUS_KEY"),
+			cWalkscoreKey = System.getenv("DF_WALKSCORE_KEY") == null ? "" : System
+					.getenv("DF_WALKSCORE_KEY");
+}
