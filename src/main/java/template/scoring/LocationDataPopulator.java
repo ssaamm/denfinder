6e18078f3bf4b258d10e641fefc9d8f7a6fd81aa
ssaamm <github@samueltaylor.org>
2014-11-02 15:50:00 -0600

    Use faster GovCensus method in location populator

--- src/main/java/template/scoring/LocationDataPopulator.java
+++ src/main/java/template/scoring/LocationDataPopulator.java
@@ -4,6 +4,7 @@ import template.data.education.EducationService;
 import template.data.fipsconversion.FipsCode;
 import template.data.fipsconversion.FipsConversionService;
 import template.data.govcensus.GovCensusService;
+import template.data.govcensus.GovData;
 import template.data.publictransit.PublicTransitService;
 
 public class LocationDataPopulator {
@@ -14,14 +15,13 @@ public class LocationDataPopulator {
 		
 		ldw.setTransitScore(PublicTransitService.getTransitScore(latitude, longitude));
 		// magic number alert
-		ldw.setSchools(EducationService.getSchools(latitude, longitude, 15));
-		ldw.setMedianIncome(GovCensusService.getMedianIncome(fipsCode.state, fipsCode.county,
-				fipsCode.tract, fipsCode.blockGroup));
-		ldw.setMedianAge(GovCensusService.getMedianAge(fipsCode.state, fipsCode.county,
-				fipsCode.tract, fipsCode.blockGroup));
-		ldw.setMarriedCoupleFamilyHouseholds(GovCensusService.getMarriedCoupleFamilyHouseholds(
-				fipsCode.state, fipsCode.county, fipsCode.tract, fipsCode.blockGroup));
-		ldw.setTotalHouseholds(GovCensusService.getTotalHouseholds(fipsCode.state, fipsCode.county,
-				fipsCode.tract, fipsCode.blockGroup));
+		ldw.setSchools(EducationService.getSchools(latitude, longitude, 0, 2));
+
+		GovData govData = GovCensusService.getCensusData(fipsCode.state, fipsCode.county,
+				fipsCode.tract, fipsCode.blockGroup);
+		ldw.setMedianIncome(govData.medianIncome);
+		ldw.setMedianAge(govData.medianAge);
+		ldw.setMarriedCoupleFamilyHouseholds(govData.marriedCoupleFamilyHouseholds);
+		ldw.setTotalHouseholds(govData.totalHouseholds);
 	}
 }
