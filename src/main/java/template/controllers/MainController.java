9b4a2a7acaf6b1afc034d83f0651043cf8ec6894
ssaamm <github@samueltaylor.org>
2014-11-29 11:46:26 -0600

    Remove vestigial code

--- src/main/java/template/controllers/MainController.java
+++ src/main/java/template/controllers/MainController.java
@@ -8,31 +8,18 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import template.data.census.CensusService;
-import template.data.education.EducationService;
-import template.data.fipsconversion.FipsConversionService;
 import template.data.geocode.GeocodeService;
 import template.m1.LatLon;
-import template.m1.LatLonBulk;
-import template.m1.LatLonData;
 import template.scoring.LocationDataPopulator;
 import template.scoring.LocationDataWrapper;
 
 @Controller
 public class MainController {
-
 	private final int WIDTH_OF_TARGET_BOX = 1;
-
-	@RequestMapping("/")
-	public String home() {
-
-		return "index";
-	}
 
 	@RequestMapping("/map")
 	public String map() {
@@ -40,50 +27,11 @@ public class MainController {
 		return "map";
 	}
 
-	@RequestMapping("/test")
-	public String test(Model model) {
-		List<LocationDataWrapper> locationDataWrappers = new ArrayList<LocationDataWrapper>();
-		LocationDataWrapper baylor = new LocationDataWrapper(31.5472, -97.1139);
-		LocationDataWrapper cbu = new LocationDataWrapper(33.9300, -117.4250);
-		LocationDataWrapper biola = new LocationDataWrapper(33.9056, -118.0181);
-		locationDataWrappers.add(baylor);
-		locationDataWrappers.add(cbu);
-		locationDataWrappers.add(biola);
-
-		for (LocationDataWrapper ldw : locationDataWrappers) {
-			LocationDataPopulator.populate(ldw);
-		}
-
-		model.addAttribute("things", locationDataWrappers);
-		return "test";
-	}
-
-	@RequestMapping("education")
-	public String education(Model model) {
-		model.addAttribute("schools",
-				EducationService.getSchools(31.5472, -97.1139, 0, 2));
-		return "education";
-	}
-
-	@RequestMapping("census")
-	public String census(Model model) {
-		model.addAttribute("places", CensusService
-				.getPlaces(FipsConversionService.getCountyFipsCode(31.5472,
-						-97.1139)));
-		return "census";
-	}
-
 	// Weird thing: this still POSTs to /m2
-	@RequestMapping(value = "/m3", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String m3input(Model model) {
 		model.addAttribute("ideal", new LocationDataWrapper());
 		return "m3input";
-	}
-
-	@RequestMapping(value = "/m2", method = RequestMethod.GET)
-	public String m2input(Model model) {
-		model.addAttribute("ideal", new LocationDataWrapper());
-		return "m2input";
 	}
 
 	@RequestMapping(value = "/m2", method = RequestMethod.POST)
@@ -125,56 +73,5 @@ public class MainController {
 
 		model.addAttribute("locationDataWrappers", locationDataWrappers);
 		return "m2submit";
-	}
-
-	@RequestMapping(value = "/m1", method = RequestMethod.GET)
-	public String m1input(Model model) {
-		model.addAttribute("latLonForm", new LatLon());
-		model.addAttribute("latLonBulk", new LatLonBulk());
-		return "m1input";
-	}
-
-	@RequestMapping(value = "/m1", method = RequestMethod.POST)
-	public String m1submit(@ModelAttribute LatLon latLon, Model model) {
-		List<LatLonData> latLons = new ArrayList<LatLonData>();
-		if (latLon == null) {
-			latLon = new LatLon();
-		}
-		String fipsCode = FipsConversionService.getCountyFipsCode(
-				latLon.getLatitude(), latLon.getLongitude());
-		LatLonData toAdd = new LatLonData(latLon,
-				CensusService.getPlaces(fipsCode), EducationService.getSchools(
-						latLon, 5));
-		latLons.add(toAdd);
-		model.addAttribute("latLons", latLons);
-		return "m1submit";
-	}
-
-	@RequestMapping(value = "/m1bulk", method = RequestMethod.POST)
-	public String m1submitbulk(@ModelAttribute LatLonBulk latLonBulk,
-			Model model) {
-		model.addAttribute("message", latLonBulk.getInput());
-		String[] lines = latLonBulk.getInput().split("\n");
-		List<LatLonData> latLons = new ArrayList<LatLonData>();
-		for (String line : lines) {
-			if (line.isEmpty()) {
-				continue;
-			}
-			if (!line.contains(" ")) {
-				continue;
-			}
-			String[] split = line.trim().split("\\s+");
-			Double lat = new Double(split[0]);
-			Double lon = new Double(split[1]);
-			LatLon latLon = new LatLon(lat, lon);
-			String fipsCode = FipsConversionService.getCountyFipsCode(
-					latLon.getLatitude(), latLon.getLongitude());
-			LatLonData toAdd = new LatLonData(latLon,
-					CensusService.getPlaces(fipsCode),
-					EducationService.getSchools(latLon, 5));
-			latLons.add(toAdd);
-		}
-		model.addAttribute("latLons", latLons);
-		return "m1submit";
 	}
 }
